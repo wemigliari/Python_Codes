a5de66a947330b7886d55b51ece1ae80e1705651
Wellington Migliari <49842956+wemigliari@users.noreply.github.com>
2020-12-23 23:18:30 +0100

    Add files via upload

---
+++ class_import_city.py
@@ -0,0 +1,13 @@
+from classes_objects_city import City ### There is another file in the folder named classes_objects
+
+place1 = City("Barcelona", 1.6, "Spain", True)
+place2 = City("Vingåker", 0.0015, "Sweden", False)
+place3 = City("Stockholm", 2.2, "Sweden", True)
+
+print(place1.name_ct)
+print(place1.metro)
+print(place3.population)
+
+print(place1)
+print(place2)
+print(place3)
