64d02b6f8e5781839fabb40f1c851a838b2a50be
Wellington Migliari <49842956+wemigliari@users.noreply.github.com>
2020-12-23 23:18:46 +0100

    Add files via upload

---
+++ classes_objects_city.py
@@ -0,0 +1,10 @@
+class City:
+
+    def  __init__(self, name_ct, population, country, metro):
+        self.name_ct = name_ct
+        self.population = population
+        self.country = country
+        self.metro = metro
+
+    def __str__(self):
+        return str(self.__class__) + ": " + str(self.__dict__)
