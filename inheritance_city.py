458a35a8b152a9f7baa83da5fade6f376713f2ed
Wellington Migliari <49842956+wemigliari@users.noreply.github.com>
2020-12-24 22:39:32 +0100

    Add files via upload

---
+++ inheritance_city.py
@@ -0,0 +1,13 @@
+class metropolitan_city_pop:
+
+    def metro_city(self):
+        print("Cities - Metropolitan areas are territories under the conurbation phenomenon.")
+
+    def pop_city(self):
+        print("Cities - People facing unbalanced distribution of wealth.")
+
+# First file with the city class
+
+metro_area = metropolitan_city_pop()
+metro_area.pop_city()
+metro_area.metro_city()
