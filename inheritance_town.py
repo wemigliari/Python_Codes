f1ac7173b5b24190c27f7a07810578c84ee80a8f
Wellington Migliari <49842956+wemigliari@users.noreply.github.com>
2020-12-25 17:08:02 +0100

    Add files via upload

---
+++ inheritance_town.py
@@ -0,0 +1,17 @@
+class metropolitan_town_pop:
+
+    def metro_town(self):
+        print("Towns - Metropolitan town areas are territories under the threat of the conurbation phenomenon.")
+
+    def pop_town(self):
+        print("Towns - People menaced by an unbalanced distribution of wealth.")
+
+    def cultural_town(self):
+        print("Towns - Although dealing with two negative fronts, towns have a stronger cultural tissue.")
+
+# econd file with the town class
+
+metro_area1 = metropolitan_town_pop()
+metro_area1.pop_town()
+metro_area1.metro_town()
+metro_area1.cultural_town()
