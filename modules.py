24686ccff3a91d59188726644ea362f3b68b4871
Wellington Migliari <49842956+wemigliari@users.noreply.github.com>
2020-12-25 17:09:16 +0100

    Add files via upload

---
+++ modules.py
@@ -0,0 +1,8 @@
+# Visit the page https://docs.python.org/3/py-modindex.html for more
+
+# Importing codes from other files
+
+import translator #Very useful tool. I can import chunks of code from separated files just to make my code cleaner.
+import calculator
+import exponent_number
+
